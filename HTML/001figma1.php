14057b3fc1d53ebbfce09967ed894afe08065afa
AbySoumareDIOUF <dioufabibatou1804@gmail.com>
2023-03-31 11:04:33 +0200

    projectSonatel

---
+++ HTML/001figma1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Exemple de barre latérale et d'en-tête</title>
+    <link rel="stylesheet" type="text/css" href="Style.css">
+  </head>
+  <body>
+<div class="general">
+
+</div>
+
+  </body>
+</html>
